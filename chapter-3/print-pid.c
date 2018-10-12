6621de2c5491bb1b564220e67afc9aa7fe9be3db
Kim Seong Jun <gmkseta@gmail.com>
2018-10-12 19:39:37 +0900

    add chap2 ,3

---
+++ chapter-3/print-pid.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main ()
+{
+  printf ("The process id is %d\n", (int) getpid ());
+  printf ("The parent process id is %d\n", (int) getppid ());
+  return 0;
+}
