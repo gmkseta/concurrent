1f271bc3c708ed94332e529479b1e95eb641660f
gmkseta <gmkseta@gmail.com>
2018-10-17 00:56:19 +0900

    report2

--- pra/report2/problemB5.c
+++ pra/report2/problemB5.c
@@ -2,21 +2,26 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-sem_t s;
-/* semaphore s */
-void *foo(void *vargp) {
+sem_t s; /* semaphore s */
+
+void *foo(void *vargp)
+{
     int id;
+    sem_wait(&s);
     id = *((int *)vargp);
-    sem_post(&s);
     printf("Thread %d\n", id);
+    sem_post(&s);
+    
 }
-int main() {
+
+int main()
+{
     pthread_t tid[2];
     int i;
-    sem_init(&s, 0, 0); /* S=0 INITIALLY */
-    for (i = 0; i < 2; i++) {
+    sem_init(&s, 0, 1); /* S=1 INITIALLY */
+    for (i = 0; i < 2; i++)
+    {
         pthread_create(&tid[i], 0, foo, &i);
-        sem_wait(&s);
     }
     pthread_join(tid[0], 0);
     pthread_join(tid[1], 0);
